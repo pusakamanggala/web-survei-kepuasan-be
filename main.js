17447b672c8ac23fb41533efcf1d0c4e0d544cf0
bily anggara <bilyanggaraa@gmail.com>
2023-04-15 19:08:32 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+const appRoute = require('./src/routes/survei-routes');
+app.use('/', appRoute);
+
+app.listen(8000, () => console.log('App running on port : 8000'));
