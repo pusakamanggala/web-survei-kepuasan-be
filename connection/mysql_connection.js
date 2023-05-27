ae1493c198c1c5368fe4c12668cfd6b18c00b485
bily anggara <bilyanggaraa@gmail.com>
2023-05-27 13:50:04 +0700

    add: db port env variable

--- connection/mysql_connection.js
+++ connection/mysql_connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements: true,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
